20f56f8ec1107fe3f112845009b883d0fa13e363
Emil H. Clausen <sequoiia@hummel.yt>
2020-10-21 15:17:59 +0200

    Reorganised main

--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,19 @@ use crate::model::{KubectlVersionResponse, AWSRssFeedResponse, Item};
 mod model;
 
 fn main() {
-    let versions = get_k8s_version().expect("Unable to get server version");
-    println!("Current server version: {}", versions.server_version.as_ref().expect("No server version").git_version.as_ref().expect("No git version key"));
+    let server_ver = get_server_k8s_version();
+    let latest_eks_version = get_latest_eks_k8s_version();
+    println!("server: {:?}", server_ver);
+    println!("latest EKS: {:?}", latest_eks_version);
 
+    if latest_eks_version > server_ver {
+        println!("Server version {} is outdated", server_ver.to_string());
+    }
+
+}
+
+fn get_latest_eks_k8s_version() -> semver::Version {
     let res = get_aws_k8s_versions().expect("Unable to get response from AWS");
-
-    let server_ver_raw = versions.server_version.as_ref().expect("No server version").git_version.as_ref().expect("No git version key");
-    let server_ver_modified : &str = server_ver_raw.chars().next().map(|c| &server_ver_raw[c.len_utf8()..]).expect("Unable to remove first character");
-    let mut server_ver = semver::Version::parse(server_ver_modified).expect("Unable to parse git version into Version");
-    server_ver = semver::Version::parse(format!("{}.{}.0", server_ver.major, server_ver.minor).as_str()).expect("Unable to parse git version into Version");
-    println!("{:?}", server_ver);
-
     let version_items : Vec<Item> = res.channel.items
         .clone()
         .into_iter()
@@ -23,22 +25,25 @@ fn main() {
         })
         .collect();
 
-    for item in &version_items {
-        println!("{}", item.title);
-        let last = item.title.split(' ').last().expect("");
-        let mut modified : String = "".to_owned();
-        if last.split('.').collect::<Vec<&str>>().len() >= 2 {
-            modified = format!("{}.0", last);
-        } else {
-            modified = last.to_owned()
-        }
-
-        let item_ver = semver::Version::parse(&modified).expect("Unable to parse item title into Version");
-        println!("{:?}", item_ver);
-        if item_ver > server_ver {
-            println!("Server version {} is outdated", server_ver.to_string());
-        }
+    let item = version_items[0].clone();
+    let last = item.title.split(' ').last().expect("");
+    let mut modified : String = "".to_owned();
+    if last.split('.').collect::<Vec<&str>>().len() >= 2 {
+        modified = format!("{}.0", last);
+    } else {
+        modified = last.to_owned();
     }
+
+    semver::Version::parse(&modified).expect("Unable to parse item title into Version")
+}
+
+fn get_server_k8s_version() -> semver::Version {
+    let versions = get_k8s_version().expect("Unable to get server version");
+
+    let server_ver_raw = versions.server_version.as_ref().expect("No server version").git_version.as_ref().expect("No git version key");
+    let server_ver_modified : &str = server_ver_raw.chars().next().map(|c| &server_ver_raw[c.len_utf8()..]).expect("Unable to remove first character");
+    let mut server_ver = semver::Version::parse(server_ver_modified).expect("Unable to parse git version into Version");
+    semver::Version::parse(format!("{}.{}.0", server_ver.major, server_ver.minor).as_str()).expect("Unable to parse git version into Version")
 }
 
 fn get_k8s_version() -> Result<KubectlVersionResponse, ()> {
