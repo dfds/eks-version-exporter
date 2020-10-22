f2fc5feaad113e0e55ff70d458a7e8c3ca364a01
Emil H. Clausen <sequoiia@hummel.yt>
2020-10-22 14:01:59 +0200

    Begun work on CI/CD pipeline

---
+++ pipeline.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+#
+# build.sh(1)
+#
+
+[[ -n $DEBUG ]] && set -x
+set -eu -o pipefail
+
+# build parameters
+readonly REGION=${AWS_DEFAULT_REGION:-"eu-central-1"}
+readonly IMAGE_NAME='eks-version-exporter'
+readonly BUILD_NUMBER=${1:-"N/A"}
+readonly BUILD_SOURCES_DIRECTORY=${2:-${PWD}}
+
+build_container_image() {
+    echo "Building container image..."
+    docker build -t ${IMAGE_NAME} -f Dockerfile .
+}
+
+push_container_image() {
+    echo "Login to docker..."
+    $(aws ecr get-login --no-include-email)
+
+    account_id=$(aws sts get-caller-identity --output text --query 'Account')
+    image_name="${account_id}.dkr.ecr.${REGION}.amazonaws.com/ded/${IMAGE_NAME}:${BUILD_NUMBER}"
+
+    echo "Tagging container image..."
+    docker tag ${IMAGE_NAME}:latest ${image_name}
+
+    echo "Pushing container image to ECR..."
+    docker push ${image_name}
+}
+
+
+build_container_image
+
+if [[ "${BUILD_NUMBER}" != "N/A" ]]; then
+    push_container_image
+fi
