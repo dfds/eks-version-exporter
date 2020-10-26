a668932e89d5b2b506611d403b8db59d71c4fd6e
Emil H. Clausen <sequoiia@hummel.yt>
2020-10-26 13:30:47 +0100

    Rearranged code; Added is_outdated label

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,31 @@
-use std::process::Command;
-use crate::model::{KubectlVersionResponse, AWSRssFeedResponse, Item};
+use crate::model::{KubectlVersionResponse, AWSRssFeedResponse, Item, State};
 use std::net::SocketAddr;
 use prometheus_exporter::prometheus::{register_gauge, Opts, register_gauge_vec, GaugeVec};
-use crate::model::github_rss::GithubFeedResponse;
 
 extern crate prometheus_exporter;
 
 mod model;
 
 fn main() {
-    let mut latest_k8s_version = get_latest_k8s_version().expect("Unable to get k8s_releases");
-    let mut eol_k8s_version = latest_k8s_version.clone();
-    eol_k8s_version.minor = eol_k8s_version.minor - 2;
-    let mut server_ver = get_server_k8s_version();
-    let mut latest_eks_version = get_latest_eks_k8s_version();
-    println!("server: {:?}", server_ver);
-    println!("latest EKS: {:?}", latest_eks_version);
-    println!("latest k8s release: {:?}", latest_k8s_version);
-    println!("oldest k8s release supported: {:?}", eol_k8s_version);
+    let mut state = State::new();
+    println!("server: {:?}", state.server_ver);
+    println!("latest EKS: {:?}", state.latest_eks_version);
+    println!("latest k8s release: {:?}", state.latest_k8s_version);
+    println!("oldest k8s release supported: {:?}", state.eol_k8s_version);
 
-    if latest_eks_version > server_ver {
-        println!("Server version {} is outdated", server_ver.to_string());
+    if state.latest_eks_version > state.server_ver {
+        println!("Server version {} is outdated", state.server_ver.to_string());
     }
 
-    let server_current_version_vec = register_gauge_vec!("eks_version_exporter", "Bunch of values", &["server_current_version", "eks_latest_available_version", "k8s_latest_available_version", "eol_latest_available_version", "last_updated", "last_updated_text"]).expect("Unable to create gauge vec");
+    let server_current_version_vec = register_gauge_vec!("eks_version_exporter", "Bunch of values", &["server_current_version", "eks_latest_available_version", "k8s_latest_available_version", "eol_latest_available_version", "last_updated", "last_updated_text", "is_outdated"]).expect("Unable to create gauge vec");
     server_current_version_vec.with_label_values(&[
-        server_ver.to_string().as_str(),
-        latest_eks_version.to_string().as_str(),
-        latest_k8s_version.to_string().as_str(),
-        eol_k8s_version.to_string().as_str(),
-        current_time_epoch().to_string().as_str(),
-        current_time_date_string().as_str()]);
+        state.server_ver.to_string().as_str(),
+        state.latest_eks_version.to_string().as_str(),
+        state.latest_k8s_version.to_string().as_str(),
+        state.eol_k8s_version.to_string().as_str(),
+        &state.current_time,
+        &state.current_time_date_string,
+        state.is_outdated.to_string().as_str()]);
 
 
     let addr_raw = "0.0.0.0:8080";
@@ -40,135 +35,25 @@ fn main() {
     loop {
         req_recv.recv().unwrap();
 
-        println!("Updating metrics");
+        println!("{}: Updating metrics", model::current_time_date_string());
+        state.refresh();
 
-        latest_k8s_version = get_latest_k8s_version().expect("Unable to get k8s_releases");
-        eol_k8s_version = latest_k8s_version.clone();
-        eol_k8s_version.minor = eol_k8s_version.minor - 2;
-        server_ver = get_server_k8s_version();
-        latest_eks_version = get_latest_eks_k8s_version();
+        if state.latest_eks_version > state.server_ver {
+            println!("{}: Server version {} is outdated", model::current_time_date_string(), state.server_ver.to_string());
+        }
+
 
         server_current_version_vec.reset();
 
         server_current_version_vec.with_label_values(&[
-            server_ver.to_string().as_str(),
-            latest_eks_version.to_string().as_str(),
-            latest_k8s_version.to_string().as_str(),
-            eol_k8s_version.to_string().as_str(),
-            current_time_epoch().to_string().as_str(),
-            current_time_date_string().as_str()]);
+            state.server_ver.to_string().as_str(),
+            state.latest_eks_version.to_string().as_str(),
+            state.latest_k8s_version.to_string().as_str(),
+            state.eol_k8s_version.to_string().as_str(),
+            &state.current_time,
+            &state.current_time_date_string,
+            state.is_outdated.to_string().as_str()]);
 
         fin_send.send(prometheus_exporter::FinishedUpdate).unwrap();
-    }
-}
-
-fn current_time_epoch() -> u128 {
-    use std::time::{SystemTime, UNIX_EPOCH};
-
-    let start = SystemTime::now();
-    let since_epoch = start.duration_since(UNIX_EPOCH).expect("Time went backwards");
-    since_epoch.as_millis()
-}
-
-fn current_time_date_string() -> String {
-    use std::time::{SystemTime, UNIX_EPOCH};
-    use chrono::prelude::DateTime;
-    use chrono::Utc;
-
-    let start = SystemTime::now();
-    let since_epoch = start.duration_since(UNIX_EPOCH).expect("Time went backwards");
-    let d = UNIX_EPOCH + since_epoch;
-
-    let datetime = DateTime::<Utc>::from(d);
-    datetime.format("%Y-%m-%d %H:%M:%S").to_string()
-}
-
-fn get_latest_eks_k8s_version() -> semver::Version {
-    let res = get_aws_k8s_versions().expect("Unable to get response from AWS");
-    let version_items : Vec<Item> = res.channel.items
-        .clone()
-        .into_iter()
-        .filter(|item| {
-            item.title.contains("Kubernetes version")
-        })
-        .collect();
-
-    let item = version_items[0].clone();
-    let last = item.title.split(' ').last().expect("");
-    let mut modified : String = "".to_owned();
-    if last.split('.').collect::<Vec<&str>>().len() >= 2 {
-        modified = format!("{}.0", last);
-    } else {
-        modified = last.to_owned();
-    }
-
-    semver::Version::parse(&modified).expect("Unable to parse item title into Version")
-}
-
-fn get_server_k8s_version() -> semver::Version {
-    let versions = get_k8s_version().expect("Unable to get server version");
-
-    let server_ver_raw = versions.server_version.as_ref().expect("No server version").git_version.as_ref().expect("No git version key");
-    let server_ver_modified : &str = server_ver_raw.chars().next().map(|c| &server_ver_raw[c.len_utf8()..]).expect("Unable to remove first character");
-    let mut server_ver = semver::Version::parse(server_ver_modified).expect("Unable to parse git version into Version");
-    semver::Version::parse(format!("{}.{}.0", server_ver.major, server_ver.minor).as_str()).expect("Unable to parse git version into Version")
-}
-
-fn get_k8s_version() -> Result<KubectlVersionResponse, ()> {
-    let output = Command::new("sh")
-        .arg("-c")
-        .arg("kubectl version -ojson")
-        .output()
-        .expect("Unable to get output");
-
-    let output_str = std::str::from_utf8(&output.stdout).expect("Unable to convert output to UTF8 str");
-
-    return match serde_json::from_str::<KubectlVersionResponse>(output_str) {
-        Ok(val) => Ok(val),
-        Err(_) => Err(())
-    };
-}
-
-fn get_aws_k8s_versions() -> Result<AWSRssFeedResponse, ()> {
-    let resp = reqwest::blocking::get("https://docs.aws.amazon.com/eks/latest/userguide/doc-history.rss").expect("Unable to get RSS response from AWS");
-    let resp_text = resp.text().expect("Unable to convert HTTP response to text");
-
-    return match serde_xml_rs::from_str(&resp_text) {
-        Ok(val) => Ok(val),
-        Err(_) => Err(())
-    }
-}
-
-fn get_k8s_releases() -> Result<GithubFeedResponse, ()> {
-    let resp = reqwest::blocking::get("https://github.com/kubernetes/kubernetes/releases.atom").expect("Unable to get RSS response from AWS");
-    let resp_text = resp.text().expect("Unable to convert HTTP response to text");
-
-    return match serde_xml_rs::from_str(&resp_text) {
-        Ok(val) => Ok(val),
-        Err(_) => Err(())
-    }
-}
-
-fn get_latest_k8s_version() -> Option<semver::Version> {
-    let k8s_releases = get_k8s_releases().expect("Unable to get k8s_releases");
-    let mut releases = Vec::new();
-    for release in &k8s_releases.entrys {
-        let modified = release.title.chars().next().map(|c| &release.title[c.len_utf8()..]).expect("Unable to remove first character");
-        let version = semver::Version::parse(modified).expect("Unable to parse version");
-        releases.push(version);
-    }
-
-    releases = releases
-        .into_iter()
-        .filter(|release| {
-            return release.pre.is_empty();
-        })
-        .collect::<Vec<semver::Version>>();
-
-    releases.sort_by_key(|x| x.minor);
-
-    match releases.last() {
-        Some(v) => Some(v.clone()),
-        None => None
     }
 }
--- src/model.rs
+++ src/model.rs
@@ -1,4 +1,77 @@
 use serde::{Serialize, Deserialize};
+use github_rss::GithubFeedResponse;
+use std::process::Command;
+
+pub struct State {
+    pub server_ver : semver::Version,
+    pub latest_eks_version : semver::Version,
+    pub latest_k8s_version : semver::Version,
+    pub eol_k8s_version : semver::Version,
+    pub current_time : String,
+    pub current_time_date_string : String,
+    pub is_outdated : i64
+}
+
+impl State {
+    pub fn new() -> Self {
+        let mut state = State {
+            server_ver: semver::Version {
+                major: 0,
+                minor: 0,
+                patch: 0,
+                pre: vec![],
+                build: vec![]
+            },
+            latest_eks_version: semver::Version {
+                major: 0,
+                minor: 0,
+                patch: 0,
+                pre: vec![],
+                build: vec![]
+            },
+            latest_k8s_version: semver::Version {
+                major: 0,
+                minor: 0,
+                patch: 0,
+                pre: vec![],
+                build: vec![]
+            },
+            eol_k8s_version: semver::Version {
+                major: 0,
+                minor: 0,
+                patch: 0,
+                pre: vec![],
+                build: vec![]
+            },
+            current_time: "".to_string(),
+            current_time_date_string: "".to_string(),
+            is_outdated: 0
+        };
+
+        state.refresh();
+        state
+    }
+
+    pub fn refresh(&mut self) {
+        self.server_ver = get_server_k8s_version();
+        self.latest_eks_version =  get_latest_eks_k8s_version();
+        self.latest_k8s_version =  get_latest_k8s_version().expect("Unable to get k8s_releases");
+        self.eol_k8s_version =  {
+            let mut x = self.latest_k8s_version.clone();
+            x.minor = x.minor - 2;
+            x
+        };
+        self.current_time =  current_time_epoch().to_string();
+        self.current_time_date_string =  current_time_date_string();
+
+        if self.latest_eks_version > self.server_ver {
+            self.is_outdated = 1;
+        } else {
+            self.is_outdated = 0;
+        }
+    }
+}
+
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct KubectlVersionResponse {
@@ -61,5 +134,118 @@ pub mod github_rss {
     #[derive(Debug, Clone, Serialize, Deserialize)]
     pub struct Entry {
         pub title : String,
+    }
+}
+
+//
+
+pub fn current_time_epoch() -> u128 {
+    use std::time::{SystemTime, UNIX_EPOCH};
+
+    let start = SystemTime::now();
+    let since_epoch = start.duration_since(UNIX_EPOCH).expect("Time went backwards");
+    since_epoch.as_millis()
+}
+
+pub fn current_time_date_string() -> String {
+    use std::time::{SystemTime, UNIX_EPOCH};
+    use chrono::prelude::DateTime;
+    use chrono::Utc;
+
+    let start = SystemTime::now();
+    let since_epoch = start.duration_since(UNIX_EPOCH).expect("Time went backwards");
+    let d = UNIX_EPOCH + since_epoch;
+
+    let datetime = DateTime::<Utc>::from(d);
+    datetime.format("%Y-%m-%d %H:%M:%S").to_string()
+}
+
+fn get_latest_eks_k8s_version() -> semver::Version {
+    let res = get_aws_k8s_versions().expect("Unable to get response from AWS");
+    let version_items : Vec<Item> = res.channel.items
+        .clone()
+        .into_iter()
+        .filter(|item| {
+            item.title.contains("Kubernetes version")
+        })
+        .collect();
+
+    let item = version_items[0].clone();
+    let last = item.title.split(' ').last().expect("");
+    let mut modified : String = "".to_owned();
+    if last.split('.').collect::<Vec<&str>>().len() >= 2 {
+        modified = format!("{}.0", last);
+    } else {
+        modified = last.to_owned();
+    }
+
+    semver::Version::parse(&modified).expect("Unable to parse item title into Version")
+}
+
+pub fn get_server_k8s_version() -> semver::Version {
+    let versions = get_k8s_version().expect("Unable to get server version");
+
+    let server_ver_raw = versions.server_version.as_ref().expect("No server version").git_version.as_ref().expect("No git version key");
+    let server_ver_modified : &str = server_ver_raw.chars().next().map(|c| &server_ver_raw[c.len_utf8()..]).expect("Unable to remove first character");
+    let mut server_ver = semver::Version::parse(server_ver_modified).expect("Unable to parse git version into Version");
+    semver::Version::parse(format!("{}.{}.0", server_ver.major, server_ver.minor).as_str()).expect("Unable to parse git version into Version")
+}
+
+pub fn get_k8s_version() -> Result<KubectlVersionResponse, ()> {
+    let output = Command::new("sh")
+        .arg("-c")
+        .arg("kubectl version -ojson")
+        .output()
+        .expect("Unable to get output");
+
+    let output_str = std::str::from_utf8(&output.stdout).expect("Unable to convert output to UTF8 str");
+
+    return match serde_json::from_str::<KubectlVersionResponse>(output_str) {
+        Ok(val) => Ok(val),
+        Err(_) => Err(())
+    };
+}
+
+fn get_aws_k8s_versions() -> Result<AWSRssFeedResponse, ()> {
+    let resp = reqwest::blocking::get("https://docs.aws.amazon.com/eks/latest/userguide/doc-history.rss").expect("Unable to get RSS response from AWS");
+    let resp_text = resp.text().expect("Unable to convert HTTP response to text");
+
+    return match serde_xml_rs::from_str(&resp_text) {
+        Ok(val) => Ok(val),
+        Err(_) => Err(())
+    }
+}
+
+fn get_k8s_releases() -> Result<GithubFeedResponse, ()> {
+    let resp = reqwest::blocking::get("https://github.com/kubernetes/kubernetes/releases.atom").expect("Unable to get RSS response from AWS");
+    let resp_text = resp.text().expect("Unable to convert HTTP response to text");
+
+    return match serde_xml_rs::from_str(&resp_text) {
+        Ok(val) => Ok(val),
+        Err(_) => Err(())
+    }
+}
+
+fn get_latest_k8s_version() -> Option<semver::Version> {
+    let k8s_releases = get_k8s_releases().expect("Unable to get k8s_releases");
+    let mut releases = Vec::new();
+    for release in &k8s_releases.entrys {
+        let modified = release.title.chars().next().map(|c| &release.title[c.len_utf8()..]).expect("Unable to remove first character");
+        let version = semver::Version::parse(modified).expect("Unable to parse version");
+        releases.push(version);
+    }
+
+    releases = releases
+        .into_iter()
+        .filter(|release| {
+            return release.pre.is_empty();
+        })
+        .collect::<Vec<semver::Version>>();
+
+    releases.sort_by_key(|x| x.minor);
+
+    match releases.last() {
+        Some(v) => Some(v.clone()),
+        None => None
     }
 }
